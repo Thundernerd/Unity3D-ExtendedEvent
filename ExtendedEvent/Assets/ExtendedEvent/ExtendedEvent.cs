555bbcf7a268e74155c7ff31d83956637953897a
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-11-04 00:38:01 +0100

    Extra types in ExtendedEvent

--- ExtendedEvent/Assets/ExtendedEvent/ExtendedEvent.cs
+++ ExtendedEvent/Assets/ExtendedEvent/ExtendedEvent.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 [Serializable]
 public sealed class ExtendedEvent {
 
+    [Serializable]
+    public class Container {
+        public UnityEngine.Object Target;
+        public int Index;
+        public MemberInfo Info = new MemberInfo();
+    }
+
+    [Serializable]
+    public class MemberInfo {
+        public int Type;
+        public string Name;
+        public int Parameters;
+        public string[] ParameterTypes;
+    }
+
     private List<Action> callbacks = new List<Action>();
 
     public void AddListener( Action callback ) {
