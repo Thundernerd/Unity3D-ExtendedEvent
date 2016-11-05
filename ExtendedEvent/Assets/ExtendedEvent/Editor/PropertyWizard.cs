a7b4f9dcdae348ffa7b4e8606be48ff21c1d7208
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-11-05 11:39:48 +0100

    Added PropertyWizard + PropertyWizardValue

---
+++ ExtendedEvent/Assets/ExtendedEvent/Editor/PropertyWizard.cs
@@ -0,0 +1,72 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace TNRD.ExtendedEvent {
+
+    public class PropertyWizard : ScriptableWizard {
+
+        public SerializedProperty Property;
+        public Action<PropertyWizardValue> Callback;
+
+        protected override sealed bool DrawWizardGUI() {
+            EditorGUILayout.PropertyField( Property, GUIContent.none );
+            return true;
+        }
+
+        void OnWizardCreate() {
+            Callback( new PropertyWizardValue() {
+                Path = Property.propertyPath,
+                Type = Property.propertyType,
+                Value = GetPropertyValue( Property )
+            } );
+        }
+
+        private object GetPropertyValue( SerializedProperty property ) {
+            switch ( property.propertyType ) {
+                case SerializedPropertyType.Generic:
+                    break;
+                case SerializedPropertyType.Integer:
+                    return property.intValue;
+                case SerializedPropertyType.Boolean:
+                    return property.boolValue;
+                case SerializedPropertyType.Float:
+                    return property.floatValue;
+                case SerializedPropertyType.String:
+                    return property.stringValue;
+                case SerializedPropertyType.Color:
+                    return property.colorValue;
+                case SerializedPropertyType.ObjectReference:
+                    return property.objectReferenceValue;
+                case SerializedPropertyType.LayerMask:
+                    break;
+                case SerializedPropertyType.Enum:
+                    break;
+                case SerializedPropertyType.Vector2:
+                    return property.vector2Value;
+                case SerializedPropertyType.Vector3:
+                    return property.vector3Value;
+                case SerializedPropertyType.Vector4:
+                    return property.vector4Value;
+                case SerializedPropertyType.Rect:
+                    return property.rectValue;
+                case SerializedPropertyType.ArraySize:
+                    break;
+                case SerializedPropertyType.Character:
+                    break;
+                case SerializedPropertyType.AnimationCurve:
+                    return property.animationCurveValue;
+                case SerializedPropertyType.Bounds:
+                    return property.boundsValue;
+                case SerializedPropertyType.Gradient:
+                    break;
+                case SerializedPropertyType.Quaternion:
+                    return property.quaternionValue;
+            }
+
+            return null;
+        }
+    }
+}
+#endif
---
+++ ExtendedEvent/Assets/ExtendedEvent/Editor/PropertyWizardValue.cs
@@ -0,0 +1,12 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace TNRD.ExtendedEvent {
+
+    public class PropertyWizardValue {
+        public SerializedPropertyType Type;
+        public string Path;
+        public object Value;
+    }
+}
+#endif
