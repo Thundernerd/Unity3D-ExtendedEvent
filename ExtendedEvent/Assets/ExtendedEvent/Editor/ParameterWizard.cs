5de741e4e327aecd94eb2ab095b8d8495027bafe
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-11-06 15:10:56 +0100

    Added ParameterWizard + ParameterWizardValue

---
+++ ExtendedEvent/Assets/ExtendedEvent/Editor/ParameterWizard.cs
@@ -0,0 +1,54 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace TNRD.ExtendedEvent {
+
+    public class ParameterWizard : ScriptableWizard {
+
+        public SerializedProperty Property;
+        public string Path;
+        public ParameterInfo[] Parameters;
+        public Action<ParameterWizardValue> Callback;
+
+        private List<FakeObject> fakeList;
+        private List<SerializedObject> serializedList;
+
+        protected override sealed bool DrawWizardGUI() {
+            if ( fakeList == null ) {
+                fakeList = new List<FakeObject>();
+                serializedList = new List<SerializedObject>();
+
+                for ( int i = 0; i < Parameters.Length; i++ ) {
+                    fakeList.Add( CreateInstance<FakeObject>() );
+                    serializedList.Add( new SerializedObject( fakeList[i] ) );
+                    var type = Parameters[i].ParameterType;
+                    var element = Property.GetArrayElementAtIndex( i );
+                    var prop = Utilities.GetPropertyFromType( type, element );
+                    Utilities.CopyValue( prop, serializedList[i].FindProperty( "Value" ), type );
+                }
+            }
+
+            for ( int i = 0; i < serializedList.Count; i++ ) {
+                var par = Parameters[i];
+                var prop = serializedList[i].FindProperty( "Value" );
+                var p = Utilities.GetPropertyFromType( par.ParameterType, prop );
+                EditorGUILayout.PropertyField( p, new GUIContent( par.Name ), false );
+            }
+
+            return true;
+        }
+
+        void OnWizardCreate() {
+            Callback( new ParameterWizardValue() {
+                Parameters = Parameters,
+                Objects = serializedList,
+                Path = Path
+            } );
+        }
+    }
+}
+#endif
---
+++ ExtendedEvent/Assets/ExtendedEvent/Editor/ParameterWizardValue.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+
+namespace TNRD.ExtendedEvent {
+
+    public class ParameterWizardValue {
+        public ParameterInfo[] Parameters;
+        public List<SerializedObject> Objects;
+        public string Path;
+    }
+}
+
+#endif
