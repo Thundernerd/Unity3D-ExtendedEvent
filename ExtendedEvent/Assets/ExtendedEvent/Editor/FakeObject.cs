574fd4e4e6287c064306df169fb8a44b2e7b8dbd
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-11-06 15:10:21 +0100

    Added FakeObject

---
+++ ExtendedEvent/Assets/ExtendedEvent/Editor/FakeObject.cs
@@ -0,0 +1,11 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace TNRD.ExtendedEvent {
+
+    public class FakeObject : ScriptableObject {
+        public global::ExtendedEvent.Value Value = new global::ExtendedEvent.Value();
+    }
+}
+
+#endif
