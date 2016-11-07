01cec8f5039ffb9c38f32ae1c8da40e17ececa8b
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-11-07 11:13:04 +0100

    Invoking based on type + Method invoke

--- ExtendedEvent/Assets/ExtendedEvent/ExtendedEvent.cs
+++ ExtendedEvent/Assets/ExtendedEvent/ExtendedEvent.cs
@@ -60,6 +60,112 @@ public sealed class ExtendedEvent {
     }
 
     public void Invoke() {
+        foreach ( var item in persistentCallbacks ) {
+            switch ( item.Info.Type ) {
+                case 1:
+                    InvokeField( item );
+                    break;
+                case 2:
+                    InvokeProperty( item );
+                    break;
+                case 3:
+                    InvokeMethod( item );
+                    break;
+            }
+        }
 
+        foreach ( var item in callbacks ) {
+            item();
+        }
+    }
+
+    private void InvokeField( Container item ) {
+
+    }
+
+    private void InvokeProperty( Container item ) {
+
+    }
+
+    private void InvokeMethod( Container item ) {
+        var target = item.Target;
+        if ( target is GameObject ) {
+            target = ( (GameObject)target ).GetComponent( item.Info.DeclaringType );
+        }
+
+        if ( target == null ) {
+            Debug.Log( "Component not found" );
+            return;
+        }
+
+        var type = target.GetType();
+
+        var types = new Type[item.Info.Parameters];
+        for ( int i = 0; i < item.Info.Parameters; i++ ) {
+            types[i] = Type.GetType( item.Info.ParameterTypes[i] );
+        }
+
+        var method = type.GetMethod( item.Info.Name, types );
+
+        if ( method == null ) {
+            Debug.Log( "Method not found" );
+            return;
+        }
+
+        if ( types.Length == 0 ) {
+            method.Invoke( target, null );
+        } else {
+            var values = new object[types.Length];
+
+            for ( int i = 0; i < values.Length; i++ ) {
+                values[i] = GetValue( types[i], item.Values[i] );
+            }
+
+            method.Invoke( target, values );
+        }
+    }
+
+    private object GetValue( Type type, Value value ) {
+        if ( type == typeof( int ) ) {
+            return value.intValue;
+        } else if ( type == typeof( bool ) ) {
+            return value.boolValue;
+        } else if ( type == typeof( float ) ) {
+            return value.floatValue;
+        } else if ( type == typeof( string ) ) {
+            return value.stringValue;
+        } else if ( type == typeof( Color ) ) {
+            return value.colorValue;
+        } else if ( IsUnityObject( type ) ) {
+            return value.objectReferenceValue;
+        } else if ( type.IsEnum ) {
+
+        } else if ( type == typeof( Vector2 ) ) {
+            return value.vector2Value;
+        } else if ( type == typeof( Vector3 ) ) {
+            return value.vector3Value;
+        } else if ( type == typeof( Vector4 ) ) {
+            return value.vector4Value;
+        } else if ( type == typeof( Rect ) ) {
+            return value.rectValue;
+        } else if ( type == typeof( AnimationCurve ) ) {
+            return value.animationCurveValue;
+        } else if ( type == typeof( Bounds ) ) {
+            return value.boundsValue;
+        } else if ( type == typeof( Quaternion ) ) {
+            return value.quaternionValue;
+        }
+
+        return null;
+    }
+
+    private bool IsUnityObject( Type type ) {
+        if ( type == null )
+            return false;
+
+        if ( type == typeof( UnityEngine.Object ) )
+            return true;
+
+        return IsUnityObject( type.BaseType );
     }
 }
